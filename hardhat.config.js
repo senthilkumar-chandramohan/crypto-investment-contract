182b84fba97b30c86c89ada2baf66f33232d9bce
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2026-03-20 12:51:24 +0530

    Removed redundant config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,9 +12,7 @@ module.exports = {
       url: "http://127.0.0.1:8545"
     },
     sepolia: {
-      url: process.env.INFURA_API_KEY 
-        ? `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
-        : process.env.RPC_URL || "",
+      url: process.env.RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111
     }
